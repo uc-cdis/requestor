df41c569e8a82807a491a536f773d0dfd231877b
Pauline <ribeyre@uchicago.edu>
2020-10-30 12:34:58 -0500

    update_request lock + refactor auth

--- src/requestor/routes/maintain.py
+++ src/requestor/routes/maintain.py
@@ -2,8 +2,7 @@ import uuid
 
 from asyncpg.exceptions import UniqueViolationError
 from datetime import datetime
-from fastapi import APIRouter, FastAPI, HTTPException, Body, Security
-from fastapi.security import HTTPAuthorizationCredentials
+from fastapi import APIRouter, Body, Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.status import (
@@ -15,9 +14,9 @@ from starlette.status import (
 )
 
 from .. import logger, arborist
-from ..auth import bearer, get_token_claims, authorize
+from ..auth import Auth
 from ..config import config
-from ..models import Request as RequestModel
+from ..models import db, Request as RequestModel
 from ..request_utils import post_status_update
 
 
@@ -38,9 +37,8 @@ class CreateRequestInput(BaseModel):
 
 @router.post("/request", status_code=HTTP_201_CREATED)
 async def create_request(
-    api_request: Request,
     body: CreateRequestInput,
-    bearer_token: HTTPAuthorizationCredentials = Security(bearer),
+    auth=Depends(Auth),
 ) -> dict:
     """
     Create a new access request.
@@ -54,9 +52,7 @@ async def create_request(
     request for the user who provided the token.
     """
     data = body.dict()
-    await authorize(
-        api_request.app.arborist_client, bearer_token, "create", [data["resource_path"]]
-    )
+    await auth.authorize("create", [data["resource_path"]])
 
     request_id = str(uuid.uuid4())
     logger.debug(f"Creating request. request_id: {request_id}. Received body: {data}")
@@ -66,7 +62,7 @@ async def create_request(
 
     if not data.get("username"):
         logger.debug("No username provided in body, using token username")
-        token_claims = await get_token_claims(bearer_token)
+        token_claims = await auth.get_token_claims()
         token_username = token_claims["context"]["user"]["name"]
         logger.debug(f"Got username from token: {token_username}")
         data["username"] = token_username
@@ -132,64 +128,72 @@ async def update_request(
     api_request: Request,
     request_id: uuid.UUID,
     status: str = Body(..., embed=True),
-    bearer_token: HTTPAuthorizationCredentials = Security(bearer),
+    auth=Depends(Auth),
 ) -> dict:
     """
     Update an access request with a new "status".
     """
-    request = await RequestModel.query.where(
-        RequestModel.request_id == request_id
-    ).gino.first_or_404()
-
-    await authorize(
-        api_request.app.arborist_client,
-        bearer_token,
-        "update",
-        [request.to_dict()["resource_path"]],
-    )
-
-    if request.status == status:
-        logger.debug(f"Request '{request_id}' already has status '{status}'")
-        return request.to_dict()
-
-    logger.debug(f"Updating request '{request_id}' with status '{status}'")
-
-    allowed_statuses = config["ALLOWED_REQUEST_STATUSES"]
-    if status not in allowed_statuses:
-        raise HTTPException(
-            HTTP_400_BAD_REQUEST,
-            f"Status '{status}' is not an allowed request status ({allowed_statuses})",
+    # only allow 1 update request at a time on the same row
+    async with db.transaction():
+        request = (
+            await RequestModel.query.where(RequestModel.request_id == request_id)
+            # lock the row by using FOR UPDATE clause
+            .with_for_update().gino.first_or_404()
         )
 
-    # the access request is approved: grant access
-    if status in config["UPDATE_ACCESS_STATUSES"]:
-        logger.debug(
-            f"Status is one of '{config['UPDATE_ACCESS_STATUSES']}', attempting to grant access in Arborist"
+        await auth.authorize(
+            "update",
+            [request.resource_path],
         )
 
-        # assume we are always granting a user access to a resource.
-        # in the future we may want to handle more use cases
-        logger.debug(f"username: {request.username}, resource: {request.resource_path}")
-        success = await arborist.grant_user_access_to_resource(
-            api_request.app.arborist_client,
-            request.username,
-            request.resource_path,
-            request.resource_display_name,
-        )
+        if request.status == status:
+            logger.debug(f"Request '{request_id}' already has status '{status}'")
+            return request.to_dict()
 
-        if not success:
-            logger.error(f"Unable to grant access. Check previous logs for errors")
+        logger.debug(f"Updating request '{request_id}' with status '{status}'")
+
+        allowed_statuses = config["ALLOWED_REQUEST_STATUSES"]
+        if status not in allowed_statuses:
             raise HTTPException(
-                HTTP_500_INTERNAL_SERVER_ERROR,
-                "Something went wrong, unable to grant access",
+                HTTP_400_BAD_REQUEST,
+                f"Status '{status}' is not an allowed request status ({allowed_statuses})",
             )
 
-    request = await (
-        RequestModel.update.where(RequestModel.request_id == request_id)
-        .values(status=status, updated_time=datetime.utcnow())
-        .returning(*RequestModel)
-        .gino.first()
-    )
+        # the access request is approved: grant access
+        if status in config["UPDATE_ACCESS_STATUSES"]:
+            logger.debug(
+                f"Status is one of '{config['UPDATE_ACCESS_STATUSES']}', attempting to grant access in Arborist"
+            )
+
+            # assume we are always granting a user access to a resource.
+            # in the future we may want to handle more use cases
+            logger.debug(
+                f"username: {request.username}, resource: {request.resource_path}"
+            )
+            success = await arborist.grant_user_access_to_resource(
+                api_request.app.arborist_client,
+                request.username,
+                request.resource_path,
+                request.resource_display_name,
+            )
+
+            if not success:
+                logger.error(f"Unable to grant access. Check previous logs for errors")
+                raise HTTPException(
+                    HTTP_500_INTERNAL_SERVER_ERROR,
+                    "Something went wrong, unable to grant access",
+                )
+
+        request = await (
+            RequestModel.update.where(RequestModel.request_id == request_id)
+            .values(status=status, updated_time=datetime.utcnow())
+            .returning(*RequestModel)
+            .gino.first()
+        )
+
+    # release the connection early, `post_status_update` could take time
+    # https://python-gino.org/docs/en/1.0/reference/extensions/starlette.html#lazy-connection
+    await api_request["connection"].release(permanent=False)
 
     res = request.to_dict()
 
@@ -203,22 +207,21 @@ async def update_request(
 
 @router.delete("/request/{request_id}", status_code=HTTP_200_OK)
 async def delete_request(
-    api_request: Request,
     request_id: uuid.UUID,
-    bearer_token: HTTPAuthorizationCredentials = Security(bearer),
+    auth=Depends(Auth),
 ) -> dict:
     """
     Delete an access request.
     """
+    # try to delete in a transaction, if no access, roll back => only 1 query
+    # transaction.raise_rollback()
     request = await RequestModel.query.where(
         RequestModel.request_id == request_id
     ).gino.first_or_404()
 
-    await authorize(
-        api_request.app.arborist_client,
-        bearer_token,
+    await auth.authorize(
         "delete",
-        [request.to_dict()["resource_path"]],
+        [request.resource_path],
     )
 
     logger.debug(f"Deleting request '{request_id}'")
