072f97e3875dd3ee7ea8ba1eb4145a683d819b7c
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-11-01 10:43:19 -0600

    using python nginx al base image

--- dockerrun.bash
+++ dockerrun.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 nginx
-gunicorn -c "/requestor/deployment/wsgi/gunicorn.conf.py" requestor.asgi:app
+poetry run gunicorn -c "/requestor/deployment/wsgi/gunicorn.conf.py" requestor.asgi:app
